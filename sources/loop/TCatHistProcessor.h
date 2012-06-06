f5f7b12fbf83620a9d96e4884fd5947a501574d7
Shinsuke OTA <ota@cns.s.u-tokyo.ac.jp>
2012-06-06 09:48:55 +0900

    daily commit   template for histgram processor added

---
+++ sources/loop/TCatHistProcessor.h
@@ -0,0 +1,22 @@
+/* $Id:$ */
+/**
+ * @file   TCatHistProcessor.h
+ * @date   Created : Jun 06, 2012 06:06:07 JST
+ *   Last Modified : 
+ * @author Shinsuke OTA <ota@cns.s.u-tokyo.ac.jp>
+ *  
+ *  
+ *    Copyright (C)2012
+ */
+#ifndef TCATHISTPROCESSOR_H
+#define TCATHISTPROCESSOR_H
+
+class TCatHistProcessor  : public TCatProcessor {
+
+public:
+   TCatHistProcessor();
+   ~TCatHistProcessor();
+
+   ClassDef("TCatHistProcessor",1);
+};
+#endif // end of #ifdef TCATHISTPROCESSOR_H
