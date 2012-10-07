51b64a1d3026f7a7ceae313f91ceef341ec7703c
Shinsuke OTA <ota@cns.s.u-tokyo.ac.jp>
2012-10-07 12:31:15 +0900

    TCatParmaeterTable class is added, which is derived from TGraph with some functions.

---
+++ sources/core/TCatParameterTable.cc
@@ -0,0 +1,59 @@
+/* $Id:$ */
+/**
+ * @file   TCatParameterTable.cc
+ * @date   Created : Oct 07, 2012 11:07:08 JST
+ *   Last Modified : 
+ * @author Shinsuke OTA <ota@cns.s.u-tokyo.ac.jp>
+ *  
+ *  
+ *    Copyright (C)2012
+ */
+#include "TCatParameterTable.h"
+
+#include <TMath.h>
+
+TCatParameterTable::TCatParameterTable()
+{
+}
+TCatParameterTable::~TCatParameterTable()
+{
+}
+
+TCatParameterTable::TCatParameterTable(const char* filename, const char *format, Option_t *opt, Bool_t xIsFirst)
+   : TGraph(filename,format,opt), fFilename(filename), fFormat(format), fXIsFirst(xIsFirst)
+{
+   if (!xIsFirst) {
+      // shoud invert x and y
+      for (Int_t i=0; i<fNpoints; i++) {
+         Double_t temp = fX[i];
+         fX[i] = fY[i];
+         fY[i] = temp;
+      }
+   }
+   
+
+   // sorting to make spline function
+   std::vector<Double_t> xsort(fNpoints);
+   std::vector<Double_t> ysort(fNpoints);
+   std::vector<Int_t> indxsort(fNpoints);
+   TMath::Sort(fNpoints, fX, &indxsort[0], false);
+   for (Int_t i = 0; i < fNpoints; ++i) {
+      xsort[i] = fX[ indxsort[i] ];
+      ysort[i] = fY[ indxsort[i] ];
+   }
+
+   // spline interpolation creating a new spline
+   fSplineXY = new TSpline3("", &xsort[0], &ysort[0], fNpoints);
+   fSplineYX = new TSpline3("", &ysort[0], &xsort[0], fNpoints);
+}
+
+void TCatParameterTable::ScaleY(const Double_t &scale)
+{
+   fScaleY = scale;
+
+   for (Int_t ix = 0; ix != fNpoints; ix++) {
+      fY[ix] *= fScaleY;
+   }
+}
+
+
---
+++ sources/core/TCatParameterTable.h
@@ -0,0 +1,49 @@
+/* $Id:$ */
+/**
+ * @file   TCatParameterTable.h
+ * @date   Created : Oct 07, 2012 11:07:51 JST
+ *   Last Modified : 
+ * @author Shinsuke OTA <ota@cns.s.u-tokyo.ac.jp>
+ *  
+ *  
+ *    Copyright (C)2012
+ */
+#ifndef TCATPARAMETERTABLE_H
+#define TCATPARAMETERTABLE_H
+
+#include <TGraph.h>
+#include <TSpline.h>
+
+
+class TCatParameterTable  : public TGraph {
+
+public:
+   TCatParameterTable();
+   virtual ~TCatParameterTable();
+
+
+   TCatParameterTable(const char* filename, const char* format, Option_t *option, Bool_t xIsFirst = kTRUE);
+
+   void ScaleY(const Double_t &scale);
+
+   virtual Double_t EvalY(const Double_t &x) const { return fSplineXY->Eval(x); }
+   virtual Double_t EvalX(const Double_t &y) const { return fSplineYX->Eval(y); }
+
+   // get filename
+   const char* GetFilename() const { return fFilename.Data(); }
+
+protected:
+   virtual void SetPoint(Int_t i, Double_t x, Double_t y) { TGraph::SetPoint(i,x,y); }
+   
+
+private:
+   TString  fFilename; // filename
+   TString  fFormat;   // format string
+   Double_t fScaleY;
+   Bool_t   fXIsFirst;
+   TSpline* fSplineXY;   // spline to evaluate Y from X
+   TSpline* fSplineYX;   // spline to evaluate X from Y
+   
+   ClassDef(TCatParameterTable,1);
+};
+#endif // end of #ifdef TCATPARAMETERTABLE_H
