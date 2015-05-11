05c6cd919825cf35776ed28778717cf1e0114b56
Shinsuke OTA <ota@cns.s.u-tokyo.ac.jp>
2015-05-12 07:30:50 +0900

    [#13] TCatCmdSave: default directory and automatic naming implemented

--- sources/commands/TCatCmdSave.cc
+++ sources/commands/TCatCmdSave.cc
@@ -2,7 +2,7 @@
 /**
  * @file   TCatCmdSave.cc
  * @date   Created : Oct 01, 2012 13:01:21 JST
- *   Last Modified : 
+ *   Last Modified : May 03, 2015 17:47:07 JST
  * @author Shinsuke OTA <ota@cns.s.u-tokyo.ac.jp>
  *  
  *  
@@ -11,8 +11,11 @@
 #include "TCatCmdSave.h"
 
 #include "TCatPadManager.h"
+#include <TSystem.h>
+#include <TTimeStamp.h>
 
 TCatCmdSave::TCatCmdSave()
+   : fDefaultDirectory(""), fHasDateFormat(kFALSE)
 {
    SetName("save");
    SetTitle("save canvas to file");
@@ -29,15 +32,46 @@ TCatCmdSave* TCatCmdSave::Instance()
 
 Long_t TCatCmdSave::Cmd(vector<TString> tokens)
 {
-   if (tokens.size()!=2) {
+   if (tokens.size()>2) {
       Help();
       return 1;
    }
-   return Run(tokens[1]);
+   TString filename = (tokens.size()==1)? "" : tokens[1];
+   return Run(filename);
 }
 
-Long_t TCatCmdSave::Run(TString &filename)
+Long_t TCatCmdSave::Run(const char* filename)
 {
-   TCatPadManager::Instance()->GetCanvas()->SaveAs(filename);
+   TString dir;
+   TTimeStamp ts;
+   if (GetAddDateDir()) {
+      dir = fDefaultDirectory;
+      dir += "/";
+      // use non UTC date
+      dir += ts.GetDate(kFALSE);
+   } else if (HasDateFormat()) {
+      // default directory name has date format
+      dir = 
+      dir = gSystem->GetFromPipe(TString::Format("date +%s",fDefaultDirectory.Data()));
+   } else {
+      dir = fDefaultDirectory;
+   }
+
+   gSystem->Exec(TString::Format("mkdir -p %s",dir.Data()));
+
+   TString name = filename;
+   std::vector<TString> files;
+   if (!name.IsNull()) {
+      TCatPadManager::Instance()->GetCanvas()->SaveAs(dir+filename);      
+   } else if (GetAutoName()) {
+      TString basename = TString::Format("%d",ts.GetSec());
+       for (Int_t i=0; i!=fFormat.size(); i++) {
+         TString f = TString::Format("%s/%s.%s",dir.Data(),basename.Data(),fFormat[i].Data());
+         printf("%s\n",f.Data());
+         TCatPadManager::Instance()->GetCanvas()->SaveAs(f);
+      }
+   } else {
+      Error("Run","file name is not given");
+   }
    return 1;
 }
--- sources/commands/TCatCmdSave.h
+++ sources/commands/TCatCmdSave.h
@@ -2,9 +2,35 @@
 /**
  * @file   TCatCmdSave.h
  * @date   Created : Oct 01, 2012 13:01:07 JST
- *   Last Modified : Feb 13, 2014 17:20:06 JST
+ *   Last Modified : May 03, 2015 17:43:41 JST
  * @author Shinsuke OTA <ota@cns.s.u-tokyo.ac.jp>
- *  
+ *
+ * TCatCmdSave
+ * 
+ * save [filename]
+ *
+ * Save canvas drawn by using ht commands.
+ *
+ * If no filename is given, AutoName flag is set and the file
+ * format(s) is given, then the file is automatically named using
+ * epoch.
+ * In order to enable this feature, please call the functions as indicated below.
+ *
+   TCatCmdSave * cmdsave = TCatCmdSave::Instance();
+   // set default directory
+   cmdsave->SetDefaultDirectory("figs");
+   // set flag to add the date directory to the default directory
+   // The directory figs/20150503 is default directory on May 3rd, 2015.
+   cmdsave->SetAddDateDir(kTRUE);
+   // Set flag for automatically naming
+   cmdsave->SetAutoName(kTRUE);
+   // Add some files formats.
+   // The canvas will be saved as all of the indicated file formats.
+   cmdsave->AddFormat("png");
+   cmdsave->AddFormat("root");
+   cmdsave->AddFormat("pdf");
+ * 
+ * 
  *  
  *    Copyright (C)2012
  */
@@ -13,17 +39,42 @@
 
 #include <TCatCmd.h>
 
+
 class TCatCmdSave  : public TCatCmd {
 
 public:
+   
    TCatCmdSave();
    ~TCatCmdSave();
 
    static TCatCmdSave* Instance();
    virtual Long_t Cmd(vector<TString> tokens);
-   virtual Long_t Run(TString &filename);
+   virtual Long_t Run(const char* filename = "");
 
-   ClassDef(TCatCmdSave,1);
+   virtual void SetAddDateDir(Bool_t add = kTRUE) { fAddDateDir = add; }
+   virtual Bool_t GetAddDateDir() { return fAddDateDir; }
+   
+   virtual void SetHasDateFormat(Bool_t has = kTRUE) { fHasDateFormat = has; }
+   virtual Bool_t HasDateFormat() { return fHasDateFormat; }
+   virtual void SetDefaultDirectory(const char* dir) { fDefaultDirectory = dir; }
+   virtual const char* GetDefaultDirectory() const { return fDefaultDirectory.Data(); }
+
+   virtual void SetAutoName(Bool_t autosave = kTRUE) { fAutoName = autosave; }
+   virtual Bool_t GetAutoName() { return fAutoName; }
+
+   virtual void AddFormat(const char* suffix) { fFormat.push_back(TString(suffix)); }
+   virtual void PrintFileFormat() {;}
+      
+
+protected:
+   TString fDefaultDirectory; // Directory to save
+   Bool_t fAddDateDir; // flag for adding date dir to default directory
+   Bool_t  fHasDateFormat; // 1 if the default directory has date format
+   Bool_t  fAutoName; // flag for automatic naming
+   std::vector<TString> fFormat; // list of file format
+   
+
+   ClassDef(TCatCmdSave,1); // save current canvas
 
 };
 #endif // end of #ifdef TCATCMDSAVE_H
