2a6f7d454f1f92c58ae863a70cbade0870b12b84
Shinsuke OTA <ota@cns.s.u-tokyo.ac.jp>
2017-01-24 15:47:24 +0900

    [mod] Any canvas can be selected if auto save is on

--- sources/commands/TCatCmdSave.cc
+++ sources/commands/TCatCmdSave.cc
@@ -2,7 +2,7 @@
 /**
  * @file   TCatCmdSave.cc
  * @date   Created : Oct 01, 2012 13:01:21 JST
- *   Last Modified : May 03, 2015 17:47:07 JST
+ *   Last Modified : 2017-01-18 17:04:21 JST (ota)
  * @author Shinsuke OTA <ota@cns.s.u-tokyo.ac.jp>
  *  
  *  
@@ -13,6 +13,7 @@
 #include "TCatPadManager.h"
 #include <TSystem.h>
 #include <TTimeStamp.h>
+#include <TROOT.h>
 
 TCatCmdSave::TCatCmdSave()
    : fDefaultDirectory(""), fHasDateFormat(kFALSE)
@@ -51,7 +52,6 @@ Long_t TCatCmdSave::Run(const char* filename)
       dir += ts.GetDate(kFALSE);
    } else if (HasDateFormat()) {
       // default directory name has date format
-      dir = 
       dir = gSystem->GetFromPipe(TString::Format("date +%s",fDefaultDirectory.Data()));
    } else {
       dir = fDefaultDirectory;
@@ -61,15 +61,25 @@ Long_t TCatCmdSave::Run(const char* filename)
 
    TString name = filename;
    std::vector<TString> files;
-   if (!name.IsNull()) {
-      TCatPadManager::Instance()->GetCanvas()->SaveAs(dir+filename);      
-   } else if (GetAutoName()) {
-      TString basename = TString::Format("%d",ts.GetSec());
-       for (Int_t i=0; i!=fFormat.size(); i++) {
-         TString f = TString::Format("%s/%s.%s",dir.Data(),basename.Data(),fFormat[i].Data());
-         printf("%s\n",f.Data());
-         TCatPadManager::Instance()->GetCanvas()->SaveAs(f);
+   if (GetAutoName()) {
+      TVirtualPad *canvas = NULL;
+      if (!name.IsNull()) {
+         canvas = (TVirtualPad*)gROOT->GetListOfCanvases()->FindObject(name);
+      } else {
+         canvas = TCatPadManager::Instance()->GetCanvas();
       }
+      if (!canvas) {
+         Error("Run","canvas is not correctly given: '%s'",name.Data());
+      } else {
+         TString basename = TString::Format("%d",ts.GetSec());
+         for (Int_t i=0; i!=fFormat.size(); i++) {
+            TString f = TString::Format("%s/%s.%s",dir.Data(),basename.Data(),fFormat[i].Data());
+            printf("%s\n",f.Data());
+            canvas->SaveAs(f);
+         }
+      }
+   } else if (!name.IsNull()) {
+      TCatPadManager::Instance()->GetCanvas()->SaveAs(dir+filename);      
    } else {
       Error("Run","file name is not given");
    }
