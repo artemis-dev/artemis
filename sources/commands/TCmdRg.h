1b6a1079e4ddb9b931d4a682a6503106cbb8dd59
Shinsuke OTA <ota@cns.s.u-tokyo.ac.jp>
2018-04-02 14:10:29 +0900

    [fix] const modifier for TH1::GetXAxis etc.

--- sources/commands/TCmdRg.h
+++ sources/commands/TCmdRg.h
@@ -3,7 +3,7 @@
  * @brief  command for setting axis range
  *
  * @date   Created       : 2016-10-09 13:25:34 JST
- *         Last Modified : 2018-02-13 17:08:50 JST (ota)
+ *         Last Modified : 2018-04-02 14:09:26 JST (ota)
  * @author KAWASE Shoichiro <kawase@aees.kyushu-u.ac.jp>
  * @comment
  *
@@ -33,7 +33,7 @@ public:
    void Help();
 
 private:
-   typedef TAxis* (TH1::*AxisGetter)() const;
+   typedef TAxis* (TH1::*AxisGetter)();
    AxisGetter fGetAxis; // pointer to Getter method of axis
 
    TCmdRg(const TCmdRg&);            // undefined
